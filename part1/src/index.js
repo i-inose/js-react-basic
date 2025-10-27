5b70d8d9ef40e17770e31b1ae8d2f90d8e608465
i-inose <i.inose0304@gmail.com>
2025-10-27 18:15:17 +0900

    Add JavaScriptBasic part1/src files

---
+++ part1/src/index.js
@@ -0,0 +1,35 @@
+// const,letの変数宣言
+
+// var val1 = "var変数";
+// console.log(val1);
+
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// var val1 = "var変数を際宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// const val4 = {
+//     name: "じゃけえ",
+//     age: 31,
+// };
+
+// console.log(val4);
+
+// val4.name = "伊野瀬";
+// val4.age = 23;
+// console.log(val4);
+
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
